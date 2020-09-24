eef6c8b444335ef996630ebddfaaa8c16d87e75e
Sri vatsan <srivatsan2607@gmail.com>
2020-09-24 15:29:24 +0530

    add star in comments

--- models/commentModel.js
+++ models/commentModel.js
@@ -3,15 +3,16 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var commentSchema = new Schema({
-	text:String,
-	createdAt:{type:Date,default:Date.now},
-	author:{
-		id:{
-			type:mongoose.Schema.Types.ObjectId,
+	text: String,
+	createdAt: { type: Date, default: Date.now },
+	rating: Number,
+	author: {
+		id: {
+			type: mongoose.Schema.Types.ObjectId,
 			ref: "User"
 		},
-		username:String
+		username: String
 	},
 });
 
-module.exports = mongoose.model("Comment",commentSchema);
+module.exports = mongoose.model("Comment", commentSchema);
--- routes/comments.js
+++ routes/comments.js
@@ -1,90 +1,90 @@
-var express    = require("express"),
-	router     = express.Router({mergeParams:true}),
+var express = require("express"),
+	router = express.Router({ mergeParams: true }),
 	Campground = require("../models/campgroundModel"),
-	Comment    = require("../models/commentModel"),
+	Comment = require("../models/commentModel"),
 	middleware = require("../middleware")
 
 
 //New route
-router.get("/new",middleware.isLoggedin,function(req,res){
-	Campground.findById(req.params.id,function(err,foundCamp){
-		if(err || !foundCamp){
-			req.flash("error","Campground not found!")
+router.get("/new", middleware.isLoggedin, function (req, res) {
+	Campground.findById(req.params.id, function (err, foundCamp) {
+		if (err || !foundCamp) {
+			req.flash("error", "Campground not found!")
 			res.redirect("back")
-		}else{
-			res.render("comments/newComments",{campground:foundCamp});
+		} else {
+			res.render("comments/newComments", { campground: foundCamp });
 		}
 	})
 })
 
 //Create route
-router.post("/",middleware.isLoggedin,function(req,res){
-	Campground.findById(req.params.id,function(err,foundCamp){
-		if(err || !foundCamp){
-			req.flash("error","Campground not found!")
-			res.redirect("/campgrounds/"+req.params.id)
-		}else{
-			Comment.create(req.body.comment,function(err,comment){
-				if(err || !comment){
-					req.flash("error",err.message)
-				}else{
-					comment.author.id=req.user._id;
-					comment.author.username=req.user.username;
+router.post("/", middleware.isLoggedin, function (req, res) {
+	Campground.findById(req.params.id, function (err, foundCamp) {
+		if (err || !foundCamp) {
+			req.flash("error", "Campground not found!")
+			res.redirect("/campgrounds/" + req.params.id)
+		} else {
+			Comment.create(req.body.comment, function (err, comment) {
+				if (err || !comment) {
+					req.flash("error", err.message)
+				} else {
+					comment.author.id = req.user._id;
+					comment.author.username = req.user.username;
 					comment.save();
 					foundCamp.comments.push(comment);
 					foundCamp.save();
-					req.flash("success","Comment Added!")
-					res.redirect("/campgrounds/"+req.params.id);
+					req.flash("success", "Comment Added!")
+					res.redirect("/campgrounds/" + req.params.id);
 				}
 			})
 		}
 	})
 });
 
-router.get("/:comment_id/edit",middleware.checkCommentOwnership,function(req,res){
-	Campground.findById(req.params.id,function(err,foundCamp){
-		if(err || !foundCamp){
-			req.flash("error","Campground not found!")
+router.get("/:comment_id/edit", middleware.checkCommentOwnership, function (req, res) {
+	Campground.findById(req.params.id, function (err, foundCamp) {
+		if (err || !foundCamp) {
+			req.flash("error", "Campground not found!")
 			res.redirect("back")
 		}
-		Comment.findById(req.params.comment_id,function(err,foundComment)		{
-		if(err || !foundComment){
-			req.flash("error","Comment not found!")
-			res.redirect("/campgrounds/"+req.params.id)
-		}else{
-			res.render("comments/editComments",		{campground:foundCamp,comment:foundComment});
-		}
-	})
+		Comment.findById(req.params.comment_id, function (err, foundComment) {
+			if (err || !foundComment) {
+				req.flash("error", "Comment not found!")
+				res.redirect("/campgrounds/" + req.params.id)
+			} else {
+				res.render("comments/editComments", { campground: foundCamp, comment: foundComment });
+			}
+		})
 	})
 })
 
-router.put("/:comment_id",middleware.checkCommentOwnership,function(req,res){
-	Campground.findById(req.params.id,function(err,foundCamp){
-		if(err || !foundCamp){
-			req.flash("error","Campground not found!")
+router.put("/:comment_id", middleware.checkCommentOwnership, function (req, res) {
+	Campground.findById(req.params.id, function (err, foundCamp) {
+		if (err || !foundCamp) {
+			req.flash("error", "Campground not found!")
 			res.redirect("back")
 		}
 		Comment.findByIdAndUpdate
-	(req.params.comment_id,req.body.comment,function(err,foundComment){
-		if(err || !foundComment){
-			req.flash("error","Comment not found!")
-			res.redirect("/campgrounds/"+req.params.id)
-		}else{
-			req.flash("info","Comment updated!")
-			res.redirect("/campgrounds/"+req.params.id)
-		}
+			(req.params.comment_id, req.body.comment, function (err, foundComment) {
+				if (err || !foundComment) {
+					req.flash("error", "Comment not found!")
+					res.redirect("/campgrounds/" + req.params.id)
+				} else {
+					req.flash("info", "Comment updated!")
+					res.redirect("/campgrounds/" + req.params.id)
+				}
+			})
 	})
-	})	
 });
 
-router.delete("/:comment_id",middleware.checkCommentOwnership,function(req,res){
-	Comment.findByIdAndRemove(req.params.comment_id,function(err){
-		if(err){
-			req.flash("error",err.message)
+router.delete("/:comment_id", middleware.checkCommentOwnership, function (req, res) {
+	Comment.findByIdAndRemove(req.params.comment_id, function (err) {
+		if (err) {
+			req.flash("error", err.message)
 			res.redirect("back")
 		}
-		req.flash("info","Comment deleted!")
-		res.redirect("/campgrounds/"+req.params.id);
+		req.flash("info", "Comment deleted!")
+		res.redirect("/campgrounds/" + req.params.id);
 	})
 })
 
